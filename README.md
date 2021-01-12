a82f802b51913fd620c8263e5c56c309025de567
Victor Praxedes <victor.praxedes@gmail.com>
2021-01-12 10:25:48 -0300

    wifi connection and socket server implementation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # mesh-connect
+
+* How to upload
+To upload the code to the device, use `pio run --target upload` in the PlatformIO CLI.
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,72 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-#define BLUE_LED 2
+#include <lwip/sockets.h>
+#include <esp_wifi.h>
+
+int socket_desc;
+const int num_socket_connections = 1;
+struct sockaddr_in  client;
+
+const char* ssid     = "Moni's Hotspot";
+const char* password = "moni2wifi";
+
+int InitializeServer(int domain, int type, int protocol,  int num_connections);
 
 void setup() {
   Serial.begin(115200);
-  pinMode(BLUE_LED, OUTPUT);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  socket_desc = InitializeServer(AF_INET , SOCK_STREAM , 0, num_socket_connections);
 }
 
 void loop() {
-  digitalWrite(BLUE_LED, HIGH);
-  delay(1000);
-  digitalWrite(BLUE_LED, LOW);
-  delay(1000);
+  Serial.println("Waiting for incoming connections...");
+  int new_socket, c = sizeof(struct sockaddr_in);
+
+  new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+
+  if (new_socket<0)
+  {
+    Serial.println("accept failed");
+  }
+  else
+  {
+    const char *message = "Hello Client , I have received your connection\n";
+    write(new_socket , message , strlen(message));
+  }
+}
+
+int InitializeServer(int domain, int type, int protocol, int num_connections){
+  int socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+     
+  if (socket_desc == -1){
+    Serial.println("Could not create socket");
+    return -1;
+  }
+  struct sockaddr_in  server;
+  //Prepare the sockaddr_in structure
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = INADDR_ANY;
+  server.sin_port = htons( 8888 );
+      
+  if( bind(socket_desc, (struct sockaddr *)&server , sizeof(server)) < 0 )
+  {
+    Serial.print("bind failed");
+    return -1;
+  }
+  else
+    Serial.println("bind done");
+  
+  listen(socket_desc, num_connections);
+  return socket_desc;
 }
