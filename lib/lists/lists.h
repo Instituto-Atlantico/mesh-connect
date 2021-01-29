bccaa689b827b3143b1f8288c947db591ba32e20
PietroAndrade <76005839+PietroAndrade@users.noreply.github.com>
2021-01-29 15:27:05 -0300

    Change library list

--- lib/lists/lists.h
+++ lib/lists/lists.h
@@ -14,23 +14,38 @@ struct node {
 void display(node* operationsPointer, int sizeNode);
 
 // Adiciona um membro ao inicio da lista
-void insertFront(node*& operationsPointer, int* sizeNode, std::string name, int number);
+void insertFront(node*& operationsPointer,
+                 int* sizeNode,
+                 std::string name,
+                 int number);
 
 // Adiciona um membro ao fim da lista
-void insertBehind(node*& operationsPointer, int* sizeNode, std::string name, int number,int* positionNode);
+void insertBehind(node*& operationsPointer,
+                  int* sizeNode,
+                  std::string name,
+                  int number,
+                  int* positionNode);
 
 // Adiciona um membro numa position especifica
-void insertNodePosition(node*& operationsPointer, int* sizeNode,std::string name,  int number, int position);
+void insertNodePosition(node*& operationsPointer,
+                        int* sizeNode,
+                        std::string name,
+                        int number,
+                        int position);
 
 // Remove o elemento inicial
 void deleteNodeFront(node*& operationsPointer, int* sizeNode);
 
 // Remove o elemento final
-void deleteNodeBehind(node*& operationsPointer, int* sizeNode, int* positionNode);
+void deleteNodeBehind(node*& operationsPointer,
+                      int* sizeNode,
+                      int* positionNode);
 
 // Remove um elemento por position
 void deleteNodePosition(node*& operationsPointer, int* sizeNode, int position);
 
 // Retorna o name pelo number
-std::string printNode(node*& operationsPointer, int* sizeNode,int number,int* position);
-
+std::string printNode(node*& operationsPointer,
+                      int* sizeNode,
+                      int number,
+                      int* position);
