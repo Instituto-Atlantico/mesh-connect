c23218eeefd0e27d438a5eadeeb69b9e30c531d1
PietroAndrade <76005839+PietroAndrade@users.noreply.github.com>
2021-02-22 14:42:48 -0300

    Fix clang format loramesh.h

--- include/loramesh.h
+++ include/loramesh.h
@@ -1,12 +1,12 @@
 #ifndef _LORAMESH_H_
 #define _LORAMESH_H_
 
+#include <Layer1_LoRa.h>
+#include <LoRaLayer2.h>
 #include <dataqueue.h>
 #include <layer2.h>
 #include "message.h"
 #include "router.h"
-#include <Layer1_LoRa.h>
-#include <LoRaLayer2.h>
 
 #define LORA_TASKS_CORE 0
 
